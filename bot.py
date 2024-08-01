7841640d0de02bbddea83410109caf9f8940c905
Luiggi Sperandio <luiggisperandio@gmail.com>
2024-08-01 17:17:36 -0300

    Bot first function

--- bot.py
+++ bot.py
@@ -9,8 +9,11 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        if 'bot' in message.content.lower():
+            await message.channel.send('Hello World!')
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
+
 client.run(environ['TOKEN'])
